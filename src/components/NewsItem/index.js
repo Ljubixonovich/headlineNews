0aa416567e8cf256a4d2f0b87ace8db8f082661e
Ljubixonovich <ljubomir.vukelic@gmail.com>
2020-12-21 13:59:06 +0100

    TopNews page, NewsItem component finished

---
+++ src/components/NewsItem/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from "styled-components/native";
+import Colors from '../../library/colors';
+
+const Wrapper = styled.TouchableOpacity`
+  margin: 5px;
+  padding: 5px;
+  width: 185px;
+  border: 1px solid ${Colors.gray};
+  border-radius: 5px;
+`;
+const Title = styled.Text`
+  font-weight: bold;
+`;
+const Image = styled.Image`
+  align-self: center;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  width: 60px;
+  height: 60px;
+`;
+const Description = styled.Text`
+  font-size: 13px;
+`;
+
+export default function NewsItem({
+  title = '',
+  description = '',
+  urlToImage = '',
+  onPress = () => {},
+}) {
+  return (
+    <Wrapper onPress={onPress}>
+      <Title>{title}</Title>
+      <Image source={{ uri: urlToImage }} />
+      <Description>{description}</Description>
+    </Wrapper>
+  );
+}
--- src/screens/TopNews/TopNews.js
+++ src/screens/TopNews/TopNews.js
@@ -1,8 +1,28 @@
 import React, { useEffect } from 'react';
-import { View, Text, Button } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
+import styled from "styled-components/native";
 import Header from '../../components/Header';
+import NewsItem from '../../components/NewsItem';
 import { GET_NEWS_SAGA, SET_COUNTRY_SAGA } from '../../store/actions';
+import { LetterCodes } from '../../library/letterCodes'; 
+
+const Wrapper = styled.View`
+  flex: 1;
+  position: relative;
+`;
+const ScrollView = styled.ScrollView``;
+const Title = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+  margin-top: 10px;  
+`;
+const PageWrapper = styled.View`
+  flex: 1;
+  padding: 6px;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
 
 export default function TopNews (props) {
   const { selectedCountry, articles } = useSelector(state => state.test);
@@ -10,31 +30,45 @@ export default function TopNews (props) {
 
   useEffect(() => {
     dispatch({ type: GET_NEWS_SAGA, selectedCountry, pageSize: 0 });
-  }, [])
+  }, [selectedCountry])
 
   const toggleDrawer = () => {
     props.navigation.toggleDrawer();
   }
 
-  const changeLanguageHandler = (input) => {
-    dispatch({ type: SET_COUNTRY_SAGA, selectedCountry: input })
+  const changeLanguageHandler = (selectedCountry) => {
+    dispatch({ type: SET_COUNTRY_SAGA, selectedCountry });
+  }
+
+  const goToArticlePage = (item) => () => {
+    props.navigation.navigate('Article', { article: item });
+  }
+
+  const getTitle = () => {
+    return LetterCodes.find(c => c.code === selectedCountry).country;
   }
 
   return(
-    <View style={{position: 'relative', flex: 1}}>
+    <Wrapper>
       <Header
         title='Top News'
         onMenuPress={toggleDrawer}
         changeLanguage={changeLanguageHandler}
       />
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>TopNews Screen</Text>
-        <Text>Articles length: {articles.length}</Text>
-        <Button
-          title='Article'
-          onPress={() => {props.navigation.push('Article')}}
-        />
-      </View>
-    </View>
+      <ScrollView>
+        <Title>{getTitle()}</Title>
+        <PageWrapper>
+          {articles && !!articles.length && articles.map((item, i) => (
+            <NewsItem 
+              key={i}
+              title={item.title}
+              description={item.description}
+              urlToImage={item.urlToImage}
+              onPress={goToArticlePage(item)}
+            />
+          ))}
+        </PageWrapper>
+      </ScrollView>
+    </Wrapper>
   )
 }
