3e39a196eb93a2d52bfa3ad74fc8b8d51bceae2b
Ljubixonovich <ljubomir.vukelic@gmail.com>
2022-02-04 19:22:24 +0100

    Add se lint prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-};
+  rules: {
+    'no-console': 'error',
+    'no-extra-boolean-cast': ['error', 'never'],
+    semi: ['error', 'never'],
+  },
+}
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,4 +1,4 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects'
 import {
   GET_NEWS_SAGA,
   GET_NEWS,
@@ -8,97 +8,95 @@ import {
   GET_NEWS_WITH_CATEGORIES,
   SET_COUNTRY_SAGA,
   SET_COUNTRY,
-  TOGGLE_LOADING
-} from '../actions';
-import API from '../../library/api';
-
+  TOGGLE_LOADING,
+} from '../actions'
+import API from '../../library/api'
 
 function* getNewsWithSearch(action) {
   try {
-    yield put ({ type: TOGGLE_LOADING, loading: true });
-    const selectedCountry = action.selectedCountry || 'us';
-    const selectedCategory = action.selectedCategory || '';
-    const pageSize = action.pageSize || 0;
-    const search = action.search || '';
+    yield put({ type: TOGGLE_LOADING, loading: true })
+    const selectedCountry = action.selectedCountry || 'us'
+    const selectedCategory = action.selectedCategory || ''
+    const pageSize = action.pageSize || 0
+    const search = action.search || ''
     if (!search.length) {
-      yield put ({ type: GET_NEWS_WITH_SEARCH, articlesWithSearchTerm: [] });
+      yield put({ type: GET_NEWS_WITH_SEARCH, articlesWithSearchTerm: [] })
     }
     let articles = yield call(API.getTopNews, {
       selectedCountry,
       selectedCategory,
       pageSize,
-      search
-    });
+      search,
+    })
     if (!!search) {
-      yield put({ type: GET_NEWS_WITH_SEARCH, articlesWithSearchTerm: articles });
+      yield put({ type: GET_NEWS_WITH_SEARCH, articlesWithSearchTerm: articles })
     } else {
-      yield put({ type: GET_NEWS, articles: articles });
+      yield put({ type: GET_NEWS, articles: articles })
     }
-    yield put ({ type: TOGGLE_LOADING, loading: false });
+    yield put({ type: TOGGLE_LOADING, loading: false })
   } catch (error) {
-     console.log('getNewsWithSearch saga ', error);
-     yield put ({ type: TOGGLE_LOADING, loading: false });
+    console.log('getNewsWithSearch saga ', error)
+    yield put({ type: TOGGLE_LOADING, loading: false })
   }
 }
 
 function* getNews(action) {
   try {
-    yield put ({ type: TOGGLE_LOADING, loading: true });
-    const selectedCountry = action.selectedCountry || 'us';
-    const selectedCategory = action.selectedCategory || '';
-    const pageSize = action.pageSize || 0;
+    yield put({ type: TOGGLE_LOADING, loading: true })
+    const selectedCountry = action.selectedCountry || 'us'
+    const selectedCategory = action.selectedCategory || ''
+    const pageSize = action.pageSize || 0
     let articles = yield call(API.getTopNews, {
       selectedCountry,
       selectedCategory,
-      pageSize
-    });
-    yield put({ type: GET_NEWS, articles: articles });
+      pageSize,
+    })
+    yield put({ type: GET_NEWS, articles: articles })
 
-    yield put ({ type: TOGGLE_LOADING, loading: false });
+    yield put({ type: TOGGLE_LOADING, loading: false })
   } catch (error) {
-     console.log('getNews saga ', error);
-     yield put ({ type: TOGGLE_LOADING, loading: false });
+    console.log('getNews saga ', error)
+    yield put({ type: TOGGLE_LOADING, loading: false })
   }
 }
 
-
 function* getNewsWithCategories(action) {
   try {
-    yield put ({ type: TOGGLE_LOADING, loading: true });
-    const selectedCountry = action.selectedCountry || 'us';
-    const categories = action.categories;
-    let articles =[];
-    let categoryIdentifier = '';
+    yield put({ type: TOGGLE_LOADING, loading: true })
+    const selectedCountry = action.selectedCountry || 'us'
+    const categories = action.categories
+    let articles = []
+    let categoryIdentifier = ''
     for (let i = 0; i < categories.length; i++) {
       articles = yield call(API.getTopNews, {
         selectedCountry,
         category: categories[i],
-      });
-      categoryIdentifier = `articles_${categories[i]}`;
+      })
+      categoryIdentifier = `articles_${categories[i]}`
       yield put({
         type: GET_NEWS_WITH_CATEGORIES,
         articles,
-        articlesCategory: categoryIdentifier
-      });
+        articlesCategory: categoryIdentifier,
+      })
     }
-    yield put ({ type: TOGGLE_LOADING, loading: false });
+    yield put({ type: TOGGLE_LOADING, loading: false })
   } catch (error) {
-     console.log('getNewsWithCategories saga ', error);
-     yield put ({ type: TOGGLE_LOADING, loading: false });
+    console.log('getNewsWithCategories saga ', error)
+    yield put({ type: TOGGLE_LOADING, loading: false })
   }
 }
 
 function* setCountry(action) {
   try {
-    yield put({ type: SET_COUNTRY, selectedCountry: action.selectedCountry });
+    yield put({ type: SET_COUNTRY, selectedCountry: action.selectedCountry })
   } catch (error) {
-     console.log('setCountry saga ', error);
+    console.log('setCountry saga ', error)
   }
 }
 
 export default function* rootSaga() {
-  yield takeLatest(SET_COUNTRY_SAGA, setCountry);
-  yield takeLatest(GET_NEWS_SAGA, getNews);
-  yield takeLatest(GET_NEWS_WITH_SEARCH_SAGA, getNewsWithSearch);
-  yield takeLatest(GET_NEWS_WITH_CATEGORIES_SAGA, getNewsWithCategories);
+  yield takeLatest(SET_COUNTRY_SAGA, setCountry)
+  yield takeLatest(GET_NEWS_SAGA, getNews)
+  yield takeLatest(GET_NEWS_WITH_SEARCH_SAGA, getNewsWithSearch)
+  yield takeLatest(GET_NEWS_WITH_CATEGORIES_SAGA, getNewsWithCategories)
 }
